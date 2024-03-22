4bd582dd9dadb567dd6218ca601c4917ff593a84
angelinapuri <angelina.puri@city.ac.uk>
2024-03-22 19:22:34 +0000

    .

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -30,11 +30,6 @@ public class TemporaryNode implements TemporaryNodeInterface {
     private Socket socket;
     private Writer writer;
     private BufferedReader reader;
-    private Map<String, String> networkMap;
-
-    public TemporaryNode() {
-        networkMap = new HashMap<>();
-    }
 
     public boolean start(String startingNodeName, String startingNodeAddress) {
         try {
@@ -64,71 +59,51 @@ public class TemporaryNode implements TemporaryNodeInterface {
     public boolean store(String key, String value) {
         int keyLines = key.split("\n").length;
         int valueLines = value.split("\n").length;
-
         try {
-            String keyHash = HashID.computeHashID(key);
-            FullNode closestNode = Nearest(keyHash);
-
-            Socket fullNodeSocket = new Socket(closestNode.getAddress().split(":")[0],
-                    Integer.parseInt(closestNode.getAddress().split(":")[1]));
-            Writer fullNodeWriter = new OutputStreamWriter(fullNodeSocket.getOutputStream());
-            BufferedReader fullNodeReader = new BufferedReader(
-                    new InputStreamReader(fullNodeSocket.getInputStream()));
-
-            fullNodeWriter.write("PUT? " + keyLines + " " + valueLines + "\n");
-            fullNodeWriter.write(key + "\n");
-            fullNodeWriter.write(value);
-            fullNodeWriter.flush();
+            writer.write("PUT? " + keyLines + " " + valueLines + "\n");
+            writer.write(key + "\n");
+            writer.write(value);
+            writer.flush();
 
             //Return true if the store worked
-            String response = fullNodeReader.readLine();
+            String response = reader.readLine();
             if (response.equals("SUCCESS")) {
                 return true;
-
             }
             // Return false if the store failed
             else if (response.equals("FAILED")) {
                 return false;
             }
+            else {
+                System.err.println("Unexpected response: " + response);
+                return false;
+            }
         } catch (IOException e) {
             System.err.println("IOException occurred: " + e.getMessage());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            return false;
         }
-        return false;
     }
 
 
     public String get(String key) {
+        int keyLines = key.split("\n").length;
         try {
-            int keyLines = key.split("\n").length;
-
-            String keyHash = HashID.computeHashID(key);
-            FullNode closestNode = Nearest(HashID.computeHashID(key));
-
-            Socket fullNodeSocket = new Socket(closestNode.getAddress().split(":")[0],
-                    Integer.parseInt(closestNode.getAddress().split(":")[1]));
-            Writer fullNodeWriter = new OutputStreamWriter(fullNodeSocket.getOutputStream());
-            BufferedReader fullNodeReader = new BufferedReader(
-                    new InputStreamReader(fullNodeSocket.getInputStream()));
-
-
             // Return the string if the get worked
-            fullNodeWriter.write("GET? " + keyLines + "\n");
-            fullNodeWriter.write(key + "\n");
-            fullNodeWriter.flush();
+            writer.write("GET? " + keyLines);
+            writer.write(key + "\n");
+            writer.flush();
 
-            String response = fullNodeReader.readLine();
+            String response = reader.readLine();
             // Return the string if the get worked
             if (response != null && response.startsWith("VALUE ")) {
                 return response;
             }
+
             // Return null if it didn't
-            else{
+            else {
                 return "Not implemented";
             }
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.err.println("IOException occurred: " + e.getMessage());
             return null;
         }

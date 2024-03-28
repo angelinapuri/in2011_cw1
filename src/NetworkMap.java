efc702a9686eb956c601faebc9cdcecc90d71ac6
angelinapuri <angelina.puri@city.ac.uk>
2024-03-28 09:10:57 +0000

    .

--- src/NetworkMap.java
+++ src/NetworkMap.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class NetworkMap {
     private Map<String, String> map;
@@ -9,12 +10,38 @@ public class NetworkMap {
     }
 
     public void add(String node, String address) {
-        map.put(node, address);
+        if (this.map.size() < 3) {
+            this.map.put(node, address);
+        }
     }
 
     public void remove(String node, String address) {
+
         map.remove(node, address);
     }
+
+    public void closestNodes(String node, String address) throws Exception {
+        TreeMap<Integer, String> distances = new TreeMap<>();
+        String hashID = HashID.computeHashID(node + "\n");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String newNodeName = entry.getKey();
+            String newNodeHashID = HashID.computeHashID(newNodeName + "\n");
+            int distance = HashID.computeDistance(hashID, newNodeHashID);
+            distances.put(distance, newNodeName); // Store distance along with the node name
+        }
+
+        // Print the three closest nodes
+        int count = 0;
+        for (Map.Entry<Integer, String> entry : distances.entrySet()) {
+            if (count >= 3) break; // Exit loop after finding the three closest nodes
+            int distance = entry.getKey();
+            String closestNode = entry.getValue();
+            System.out.println("Closest node: " + closestNode + ", Distance: " + distance);
+            count++;
+        }
+    }
+
 
     public String getAddress(String node) {
         return map.get(node);
--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -31,80 +31,76 @@ public class TemporaryNode implements TemporaryNodeInterface {
     private Socket socket;
     private Writer writer;
     private BufferedReader reader;
-    private NetworkMap networkMap;
-
 
     public boolean start(String startingNodeName, String startingNodeAddress) {
         try {
-            //Connect to the starting node
-            socket = new Socket(startingNodeAddress.split(":")[0], parseInt(startingNodeAddress.split(":")[1]));
+            // Connect to the starting node
+            socket = new Socket(startingNodeAddress.split(":")[0], Integer.parseInt(startingNodeAddress.split(":")[1]));
             writer = new OutputStreamWriter(socket.getOutputStream());
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            writer.write("START 1 angelina.puri@city.ac.uk:MyImplementation" + "\n");
+            // Send start message to the starting node
+            writer.write("START 1 " + startingNodeName + "\n");
             writer.flush();
 
-            // Return true if the 2D#4 network can be contacted
+            // Return true if the network can be contacted
             String response = reader.readLine();
-            // Return false if the 2D#4 network can't be contacted
-            System.out.println(response);
             return response != null && response.startsWith("START 1 ");
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.err.println("IOException occurred: " + e.getMessage());
             return false;
         }
     }
 
     public boolean store(String key, String value) {
+        int keyLines = key.split("\n").length;
+        int valueLines = value.split("\n").length;
         try {
-            String[] keyLines = key.split("\n");
-            String[] valueLines = value.split("\n");
-                writer.write("PUT? " + keyLines.length + " " + valueLines.length + "\n" + key + "\n" + value + "\n");
-                writer.flush();
+            writer.write("PUT? " + keyLines + " " + valueLines + "\n");
+            writer.write(key + "\n");
+            writer.write(value);
+            writer.flush();
 
-                //Return true if the store worked
-                String response2 = readUntilEnd(reader);
-                if (response2.startsWith("SUCCESS")) {
-                    return true;
-                }
-                // Return false if the store failed
-                else if (response2.startsWith("FAILED")) {
-                    return false;
-                }
+            //Return true if the store worked
+            String response = reader.readLine();
+            if (response.equals("SUCCESS")) {
+                return true;
+            }
+            // Return false if the store failed
+            else if (response.equals("FAILED")) {
+                return false;
+            } else {
+                System.err.println("Unexpected response: " + response);
+                return false;
+            }
         } catch (IOException e) {
             System.err.println("IOException occurred: " + e.getMessage());
             return false;
         }
-        return false;
     }
 
 
     public String get(String key) {
+        int keyLines = key.split("\n").length;
         try {
-            // Send GET request
-            String[] keyLines = key.split("\n");
-            writer.write("GET? " + keyLines.length + "\n" + key + "\n");
+            // Return the string if the get worked
+            writer.write("GET? " + keyLines);
+            writer.write(key + "\n");
             writer.flush();
 
-            // Read GET response
-            String response2 = readUntilEnd(reader);
-            if (response2.startsWith("VALUE")) {
-                return response2;
-            } else {
-                // Value not found
+            String response = reader.readLine();
+            // Return the string if the get worked
+            if (response != null && response.startsWith("VALUE ")) {
+                return response;
+            }
+
+            // Return null if it didn't
+            else {
                 return "NOPE";
             }
-        } catch (Exception e) {
-            System.err.println("Exception occurred: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IOException occurred: " + e.getMessage());
             return null;
         }
-    }
-    private String readUntilEnd(BufferedReader reader) throws IOException {
-        StringBuilder responseBuilder = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            responseBuilder.append(line).append("\n");
-        }
-        return responseBuilder.toString().trim();
     }
 }

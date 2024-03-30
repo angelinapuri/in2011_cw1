e4ed63be263f1c9deb3e524f8a10640f361d8097
angelinapuri <angelina.puri@city.ac.uk>
2024-03-30 13:53:13 +0000

    .

--- src/FullNode.java
+++ src/FullNode.java
@@ -1,20 +1,48 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
+// IN2011 Computer Networks
+// Coursework 2023/2024
+//
+// Submission by
+// YOUR_NAME_GOES_HERE
+// YOUR_STUDENT_ID_NUMBER_GOES_HERE
+// YOUR_EMAIL_GOES_HERE
 
-public class FullNode {
-    private ServerSocket serverSocket;
-    private Socket socket;
-    private BufferedReader reader;
-    private OutputStreamWriter writer;
-    private NetworkMap networkMap;
-    private DataStore dataStore;
+ import java.io.BufferedReader;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.io.OutputStreamWriter;
+ import java.io.Writer;
+ import java.net.ServerSocket;
+ import java.net.Socket;
+ import java.util.HashMap;
+ import java.util.Map;
 
-    public FullNode() {
-        networkMap = new NetworkMap();
-        dataStore = new DataStore();
-    }
+ // DO NOT EDIT starts
+ interface FullNodeInterface {
+ public boolean listen(String ipAddress, int portNumber);
+ public void handleIncomingConnections(String startingNodeName, String startingNodeAddress);
+ }
+ // DO NOT EDIT ends
 
+
+ public class FullNode implements FullNodeInterface {
+ private ServerSocket serverSocket;
+ private Socket socket;
+ private Writer writer;
+ private BufferedReader reader;
+ private NetworkMap networkMap;
+ private DataStore dataStore;
+
+ public FullNode(NetworkMap networkMap, DataStore dataStore) {
+ this.networkMap = networkMap;
+ this.dataStore = dataStore;
+ }
+
+
+ public FullNode() {
+ networkMap = new NetworkMap();
+ dataStore = new DataStore();
+
+ }
     public boolean listen(String ipAddress, int portNumber) {
         try {
             // Start listening on given port

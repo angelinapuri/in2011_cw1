b379386670a69ceb78cbab66e631a1cea049c4ff
angelinapuri <angelina.puri@city.ac.uk>
2024-03-30 13:57:08 +0000

    .

--- src/FullNode.java
+++ src/FullNode.java
@@ -109,8 +109,8 @@
 
             // Prepare and send the NOTIFY? request
             String nodeName = "angelina.puri@city.ac.uk:test-01";
-            writer.write("NOTIFY?\n" + nodeName + "\n" + (ipAddress + ":" + portNumber) + "\n");
-            System.out.println("NOTIFY?\n" + nodeName + "\n" + (ipAddress + ":" + portNumber) + "\n");
+            writer.write("NOTIFY?\n" + nodeName + "\n" + (ipAddress + ":" + portNumber));
+            System.out.println("NOTIFY?\n" + nodeName + "\n" + (ipAddress + ":" + portNumber));
             writer.flush();
 
             // Read and process the response from other nodes

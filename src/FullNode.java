a5b36939141b6a66a0ca7cea85566163adfdac52
angelinapuri <angelina.puri@city.ac.uk>
2024-03-31 21:21:09 +0100

    .

--- src/FullNode.java
+++ src/FullNode.java
@@ -141,10 +141,8 @@
          }
 
 
-         private void handleNearestRequest(String message, NetworkMap networkMap) throws IOException {
+         private void handleNearestRequest(String hashID, NetworkMap networkMap) throws IOException {
              try {
-                 String[] messageParts = message.split(" ");
-                 String hashID = messageParts[1];
                  Map<Integer, List<Node>> distances = new TreeMap<>();
 
                  // Compute distances to all nodes in the map
